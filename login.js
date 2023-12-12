90ff166145d7d47fda970e5fb2264cffe278b472
Ray Jhonatas <jhonattaray11@gmail.comgit config --global user.email jhonattaray11@gmail.com>
2023-12-12 15:17:37 -0300

    initial commit

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function fazerLogin() {
     var emailField = document.getElementById('user');
-    var passwordField= document.getElementById('password');
+    var passwordField = document.getElementById('password');
     
     if (!emailField || !passwordField) {
         console.log("Campos não encontrados.");
@@ -8,15 +8,16 @@ function fazerLogin() {
     }
 
     if (!emailField.value || !passwordField.value) {
-        alert("Por favor forneça os dados.");
+        alert("Por favor, forneça os dados.");
         return false;
     }   
     
-    if (passwordField.value.length != 6) {
-        alert("Senha incorreta. Por favor verifique e corrija.");
+    if (passwordField.value.length !== 6) {
+        alert("Senha incorreta. Por favor, verifique e corrija.");
         return false;
     }
 
-       alert("Login realizado com sucesso!");
-       return true;
+    alert("Login realizado com sucesso!");
+    window.location.href = 'C:/internship-program-ui-ray/layout.html';
+    return true;
 }
