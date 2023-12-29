e8fe2663882528f8e2c7505f9a120e06e4cacef1
Ray Jhonatas <jhonattaray11@gmail.comgit config --global user.email jhonattaray11@gmail.com>
2023-12-29 04:11:30 -0300

    commit

---
+++ banks.js
@@ -0,0 +1,38 @@
+function redirecionarParaVer() {
+    window.location.href = 'file:///C:/internship-program-ui-ray/Tela%20de%20Banco.html'; 
+}
+
+function redirecionarParaEditar() {
+    window.location.href = 'file:///C:/internship-program-ui-ray/Tela%20de%20Banco.html'; 
+}
+
+function confirmarRemover() {
+    Swal.fire({
+        title: 'Tem certeza?',
+        text: 'Você realmente deseja remover este item?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Não'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Removido!',
+                'O item foi removido com sucesso.',
+                'success'
+            );
+        } else {
+            Swal.fire(
+                'Cancelado',
+                'A remoção foi cancelada.',
+                'error'
+            );
+        }
+    });
+}
+
+function redirecionarParaPagina() {
+    window.location.href = "file:///C:/internship-program-ui-ray/Tela%20de%20Banco.html";
+}
--- layout.js
+++ layout.js
@@ -1,14 +1,14 @@
-
-function mostrarLinha() {
-    var linha = document.getElementById("linhaParaMostrar");
+function mostrarLinha(id) {
+    var linha = document.getElementById(id);
+    linha.style.borderBottom = "1px solid #000";
     linha.style.display = "inline";
 }
 
-function ocultarLinha() {
-    var linha = document.getElementById("linhaParaMostrar");
+function ocultarLinha(id) {
+    var linha = document.getElementById(id);
     linha.style.display = "none";
 }
 
-function redirecionarParaPagina() {
-    window.location.href = "file:///C:/internship-program-ui-ray/People.html";
+function redirecionarParaPagina(pagina) {
+    window.location.href = "file:///C:/internship-program-ui-ray/" + pagina;
 }
